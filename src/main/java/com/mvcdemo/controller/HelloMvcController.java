e40b2ba29e5a8ea6515ebb754acf17069d1623ab
Mike <doubimike@qq.com>
2021-04-14 23:46:06 +0800

    chore:controller ok

--- src/main/java/com/mvcdemo/controller/HelloMvcController.java
+++ src/main/java/com/mvcdemo/controller/HelloMvcController.java
@@ -1,4 +1,4 @@
-package com.mvcdemo;
+package com.mvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/mvcdemo/service/CourseService.java
@@ -0,0 +1,17 @@
+package com.mvcdemo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.mvcdemo.model.Course;
+
+@Service
+public interface CourseService {
+	
+	
+	Course getCoursebyId(Integer courseId);
+	
+
+	
+	
+
+}
